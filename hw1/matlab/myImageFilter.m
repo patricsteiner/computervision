1f1ea192e21067938a262a8fc335dca36a37b2f8
Patric Steiner <patric.steiner1@gmail.com>
2017-03-19 00:59:49 +0900

    working on hw

---
+++ hw1/matlab/myImageFilter.m
@@ -0,0 +1,45 @@
+function Iconv = myImageFilter(I, S)
+% convolves grayscale iamge I with kernel S
+
+[IWidth, IHeight] = size(I);
+[SWidth, SHeight] = size(S);
+SCenter = floor(SWidth/2+1);
+Iconv = zeros(IWidth, IHeight);
+
+for x = 1:IWidth
+    for y = 1:IHeight
+        for i = 1:SWidth
+            for j = 1:SHeight
+                col = x + i - SCenter;
+                row = y + j - SCenter;
+                % if col or row is outside the actual image, just use the
+                % nearest value instead
+                while col < 1
+                    col = col + 1;
+                end
+                while col > IWidth
+                    col = col - 1;
+                end
+                while row < 1
+                    row = row + 1;
+                end
+                while row > IHeight
+                    row = row - 1;
+                end
+                Iconv(x, y) = I(col, row) * S(i, j);
+            end
+        end
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
